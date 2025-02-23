e3b75851c4a697549cc6dbad7b677e15a0026151
ClariS <1457715339@qq.com>
2025-02-23 22:11:44 +0800

    feat: 支持 PWA

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import UnoCSS from "unocss/vite"
 import AutoImport from "unplugin-auto-import/vite"
 import Components from "unplugin-vue-components/vite"
 import { defineConfig, loadEnv } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 import svgLoader from "vite-svg-loader"
 
 // Configuring Vite: https://cn.vite.dev/config
@@ -121,6 +122,30 @@ export default defineConfig(({ mode }) => {
       Components({
         dts: "types/auto/components.d.ts",
         resolvers: [VantResolver()]
+      }),
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: {
+          name: "MOBVUE",
+          short_name: "MOBVUE",
+          theme_color: "#1989fa",
+          icons: [
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png"
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png"
+            }
+          ]
+        },
+        devOptions: {
+          enabled: true,
+          type: "module"
+        }
       })
     ],
     // Configuring Vitest: https://cn.vitest.dev/config
